3cc2529432eab301cfe03c382e2e5c453632cd6e
wyktors <wyktor93@hotmail.com>
2015-05-06 16:14:42 -0300

    pseudocódigo coloração.

--- cpp/src/algorithms/colouring_die.cc
+++ cpp/src/algorithms/colouring_die.cc
@@ -17,7 +17,14 @@ Colouring_DIE::~Colouring_DIE() {
 
 void Colouring_DIE::execute(Graph<Colour> aGraph){
 	/*
-	 * 1-
+	 * 1-renumerar vértices tal que xi seja o i-ésimo
+	 * 2-para i=0;i=numero de vertices;i++{
+	 * 	2.1-procurar vertices adjacentes a xi;
+	 * 	2.2-verificar se algum dos vertices adjacentes a xi possui a cor C{
+	 * 		2.2.1-se nenhum tiver a cor C, então xi recebe a cor C;
+	 * 		2.2.2-caso contrario, C+1 e volta a 2.2
+	 * 		}*
+	 *
 	 */
 }
 
