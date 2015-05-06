9a364676384f01413581900e23b98cee55ab6702
gian <gian.ricardo@gmail.com>
2015-05-06 16:07:59 -0300

    inicio de implementação do teste de planaridade.

---
+++ cpp/src/algorithms/planarity_test.cc
@@ -0,0 +1,21 @@
+/*
+ * planarity_test.cc
+ *
+ *  Created on: May 6, 2015
+ *      Author: gian
+ */
+
+#include "planarity_test.h"
+
+namespace graphlib {
+
+Planarity_Test::Planarity_Test() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Planarity_Test::~Planarity_Test() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace graphlib */
---
+++ cpp/src/algorithms/planarity_test.h
@@ -0,0 +1,21 @@
+/*
+ * planarity_test.h
+ *
+ *  Created on: May 6, 2015
+ *      Author: gian
+ */
+
+#ifndef PLANARITY_TEST_H_
+#define PLANARITY_TEST_H_
+
+namespace graphlib {
+
+class Planarity_Test {
+public:
+	Planarity_Test();
+	virtual ~Planarity_Test();
+};
+
+} /* namespace graphlib */
+
+#endif /* PLANARITY_TEST_H_ */
